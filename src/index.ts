ea7865a8fc6100eebc7dfb99d9a32488ff672a9c
Antonio <antoniorivera.cs18@gmail.com>
2023-06-26 20:49:55 -0400

    Testing config

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,6 @@
 import { Grid } from "./grid/Grid";
+import { Board } from "./grid/Board";
+import { Cell } from "./cell/Cell";
 import "./file.css";
 
 const canvas = <HTMLCanvasElement>document.getElementById("canvas");
@@ -9,4 +11,17 @@ const ctx = canvas.getContext("2d")!;
 const grid = new Grid(100, canvas.width, canvas.height, ctx);
 grid.Draw();
 
-//ctx.fillRect(400, 200, 100, 100);
+const board = new Board(grid);
+console.log(board);
+// ctx.fillRect(0, 400, 100, 100);
+let count = 0;
+while (count <= 10) {
+  const x = Math.floor(Math.random() * board.GetX());
+  const y = Math.floor(Math.random() * board.GetY());
+
+  const cell = new Cell(grid.GetCellSize(), [x, y], 1, ctx);
+  cell.Draw();
+
+  board.SetCell(cell);
+  count++;
+}
